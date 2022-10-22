d989ce67f5f2ac0ba8f871ffbdede3a099420228
Calvin Wu <calvin620707@msn.com>
2022-10-22 10:38:41 +0800

    add point

--- reward_chart/main/urls.py
+++ reward_chart/main/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:chart_id>/points", views.increase_points, name="increase_points"),
+    path("<int:chart_id>/points", views.redeem, name="redeem"),
 ]
--- reward_chart/main/views.py
+++ reward_chart/main/views.py
@@ -1,10 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Chart
 from datetime import datetime
 
 
+def _prepare_context(chart: Chart) -> dict:
+    return {"chart": chart, "now": datetime.now().isoformat()}
+
+
+def _render_index(request, chart):
+    return render(request, "index.html", _prepare_context(chart))
+
+
 def index(request):
-    default_chart = Chart.objects.first()
-    context = {"chart": default_chart, "now": datetime.now().isoformat()}
-    return render(request, "index.html", context)
+    chart = Chart.objects.first()
+    return _render_index(request, chart)
+
+
+def increase_points(request, chart_id):
+    chart = get_object_or_404(Chart, pk=chart_id)
+    chart.points += 1
+    chart.save()
+    return _render_index(request, chart)
+
+
+def redeem(request, chart_id):
+    chart = get_object_or_404(Chart, pk=chart_id)
+    chart.points -= 1
+    chart.save()
+    return _render_index(request, chart)
